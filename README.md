48b332a0a1e45e7320342dc5b31155e41180e2d6
Gabriel de Souza <gabrieldesouza@MacBook-Pro-2019-i9-Capa.local>
2024-02-07 22:17:56 +0000

    chore: add db config and migrations

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-fastify
+
+
+```bash
+npm run knex -- migrate:make create-transactions
+npm run knex -- migrate:latest
+npm run knex -- migrate:rollback
+```
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -6,6 +9,26 @@ app.get('/', async () => {
   return { hello: 'world' }
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transactions
+})
+
+app.get('/hello2', async () => {
+  const transactions = await knex('transactions')
+    .select('*')
+    .where('amount', '>', 1000)
+
+  return transactions
+})
+
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server running on http://localhost:3333')
 })
