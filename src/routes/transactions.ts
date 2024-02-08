d5d750f1d7d234420af93cb970f515983459432f
Gabriel de Souza <gabrieldesouza@MacBook-Pro-2019-i9-Capa.local>
2024-02-08 22:35:54 +0000

    chore: add database typing

---
+++ src/routes/transactions.ts
@@ -0,0 +1,65 @@
+import type { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+import { z } from 'zod'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transaction = await knex('transactions')
+      .select('*')
+      .where({
+        id,
+      })
+      .first()
+
+    return { transaction }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
+    return reply.status(201).send()
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,18 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
+// import crypto from 'node:crypto'
+// import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/', async () => {
-  return { hello: 'world' }
-})
+app.register(cookie)
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
-})
-
-app.get('/hello2', async () => {
-  const transactions = await knex('transactions')
-    .select('*')
-    .where('amount', '>', 1000)
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: '/transactions',
 })
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP Server running on http://localhost:3333')
+  console.log(`HTTP Server running on http://localhost:${env.PORT}`)
 })
